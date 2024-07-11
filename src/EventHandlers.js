9e84ddf33a0de3fdbe4f51bb1cac763962013338
Bradley Sandilands <86394041+Brad-is-Love@users.noreply.github.com>
2024-07-12 07:51:17 +1200

    Redeploy

--- src/EventHandlers.js
+++ src/EventHandlers.js
@@ -1,7 +1,7 @@
 /*
  *Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features*
 
- Adding comment to action new
+ Adding comment to action new deployment
  */
 
 let { SweepStakesNFTsContract } = require("../generated/src/Handlers.bs.js");
